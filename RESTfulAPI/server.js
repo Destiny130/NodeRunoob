7949d8b5e4a32e8ab5804554e215e49a73c61463
Destiny130 <destiny1307654641@hotmail.com>
2018-03-01 11:22:20 +0800

    Update server.js

--- RESTfulAPI/server.js
+++ RESTfulAPI/server.js
@@ -9,8 +9,52 @@ app.get('/listUsers', function(req, res) {
     });
 });
 
+var user = {
+    "user4": {
+        "name": "mohit",
+        "password": "password4",
+        "profession": "teacher",
+        "id": 4
+    }
+}
+
+app.get('/addUser', function(req, res) {
+    fs.readFile(__dirname + '/' + 'users.json', 'utf8', function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        data = JSON.parse(data);
+        data['user4'] = user['user4'];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});
+
+app.get('/:id', function(req, res) {
+    fs.readFile(__dirname + '/' + 'users.json', 'utf8', function(err, data) {
+        if (err) return console.error(err);
+        data = JSON.parse(data);
+        var user = data['user' + req.params.id];
+        console.log(user);
+        res.end(JSON.stringify(user));
+    });
+});
+
+app.get('/deleteUser', function(req, res) {
+    fs.readFile(__dirname + '/' + 'users.json', 'utf8', function(err, data) {
+        if (err) return console.error('error: ' + err);
+        // console.log(__dirname);
+        data = JSON.parse(data);
+        // console.log('before delete. data: ' + data);
+        delete data["user" + 2];
+        // console.log('after delete. data: ' + data);
+        res.end(JSON.stringify(data));
+    })
+})
+
 var server = app.listen(8081, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Application instance. Address is httl://%s:%s', host, port);
+    // console.log('dirname: ' + __dirname);
 });
